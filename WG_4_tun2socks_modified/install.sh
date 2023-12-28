51d5cf2a7f9573424481b8f3e317318d39ecb607
Andy <rahmanjan1989630@gmail.com>
2023-12-28 08:18:59 +0000

    a

--- WG_4_tun2socks_modified/install.sh
+++ WG_4_tun2socks_modified/install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 #下载安装tun2socks
-apt update
-apt install unzip
+
 wget https://github.com/xjasonlyu/tun2socks/releases/download/v2.5.2/tun2socks-linux-amd64.zip
 unzip tun2socks-linux-amd64.zip
 mv tun2socks-linux-amd64 /usr/bin/tun2socks
 
 #下载安装魔改wireguard
-curl -sSL https://raw.githubusercontent.com/andy72630/Wireguard_w_IPv6/main/WG_4_tun2socks_modified/wg_tun2socks_installer.sh | bash
+echo "1" | curl -sSL https://raw.githubusercontent.com/andy72630/Wireguard_w_IPv6/main/WG_4_tun2socks_modified/wg_tun2socks_installer.sh | bash
 curl -sSL https://raw.githubusercontent.com/andy72630/Wireguard_w_IPv6/main/WG_4_tun2socks_modified/monitor.sh | bash
-wget https://raw.githubusercontent.com/andy72630/Wireguard_w_IPv6/main/WG_4_tun2socks_modified/tunnel_setup.sh -O tunnel_setup.sh && bash tunnel_setup.sh start
-echo "结束时请手动运行bash tunnel_setup.sh stop"
+wget https://raw.githubusercontent.com/andy72630/Wireguard_w_IPv6/main/WG_4_tun2socks_modified/tunnel_setup.sh
+echo "请手动运行 bash tunnel_setup.sh start 或者 bash tunnel_setup.sh stop"
