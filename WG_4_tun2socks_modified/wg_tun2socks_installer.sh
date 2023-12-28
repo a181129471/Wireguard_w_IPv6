7d19c5b889bda6ab5a8933047fe6f7c092897f7e
Andy <rahmanjan1989630@gmail.com>
2023-12-28 08:24:02 +0000

    a

--- WG_4_tun2socks_modified/wg_tun2socks_installer.sh
+++ WG_4_tun2socks_modified/wg_tun2socks_installer.sh
@@ -103,98 +103,12 @@ EOF
     echo "${content}" | qrencode -o - -t UTF8
 }
 
-wireguard_remove(){
-
-    wg-quick down wg0
-    apt-get remove -y wireguard
-    rm -rf /etc/wireguard
-
-}
-
-add_user(){
-    echo -e "\033[37;41m给新用户起个名字，不能和已有用户重复\033[0m"
-    read -p "请输入用户名：" newname
-    cd /etc/wireguard/
-    cp client.conf $newname.conf
-    wg genkey | tee temprikey | wg pubkey > tempubkey
-    ipnum=$(grep Allowed /etc/wireguard/wg0.conf | tail -1 | awk -F '[ ./]' '{print $6}')
-    lanip=$(grep Address /etc/wireguard/wg0.conf | tail -1 | awk -F '[ ./]' '{print $5}')
-    newnum=$((10#${ipnum}+1))
-    sed -i 's%^PrivateKey.*$%'"PrivateKey = $(cat temprikey)"'%' $newname.conf
-    sed -i 's%^Address.*$%'"Address = 10.100.$lanip.$newnum\/24"'%' $newname.conf
-
-cat >> /etc/wireguard/wg0.conf <<-EOF
-[Peer]
-PublicKey = $(cat tempubkey)
-AllowedIPs = 10.100.$lanip.$newnum/32
-EOF
-    wg set wg0 peer $(cat tempubkey) allowed-ips 10.100.$lanip.$newnum/32
-    echo -e "\033[37;41m添加完成，文件：/etc/wireguard/$newname.conf\033[0m"
-    rm -f temprikey tempubkey
-}
-
-#开始菜单
-start_menu(){
-    if ! command -v wg &> /dev/null; then
-        # WireGuard is not installed, update and install net-tools
-        apt update -y
-        apt install net-tools -y
-    fi
-    clear
-    wireguard_install
-    echo -e "\033[43;42m ====================================\033[0m"
-    echo -e "\033[43;42m 介绍：wireguard_tun2Socks一键脚本ipv4               \033[0m"
-    echo -e "\033[43;42m 系统：Ubuntu                         \033[0m"
-    echo -e "\033[43;42m 原作者：A                              \033[0m"
-    echo -e "\033[43;42m 修改者：Andy                         \033[0m"
-    echo -e "\033[43;42m ====================================\033[0m"
-    echo
-    echo -e "\033[0;33m 1. 安装wireguard\033[0m"
-    echo -e "\033[0;33m 2. 查看客户端二维码\033[0m"
-    echo -e "\033[0;31m 3. 删除wireguard\033[0m"
-    echo -e "\033[0;33m 4. 增加用户\033[0m"
-    echo -e "0. 退出脚本"
-    echo
-    route -n -4
-    echo -e "\033[43;42m ====================================\033[0m"
-    route -n -6
-    read -p "请输入数字:" num
-    case "$num" in
-    1)
-        wireguard_install
-        start_menu
-        ;;
-    2)
-        read -p "请输入客户端用户名：" name
-        content=$(cat /etc/wireguard/$name.conf)
-        echo "${content}" | qrencode -o - -t UTF8
-        echo "按任意键继续..."
-        read -n 1
-        start_menu
-        ;;
-    3)
-        wireguard_remove
-        start_menu
-        ;;
-    4)
-        add_user
-        start_menu
-        ;;
-    0)
-        exit 1
-        ;;
-    *)
-        clear
-        echo -e "请输入正确数字"
-        sleep 2s
-        start_menu
-        ;;
-    esac
-}
-
-start_menu
 
 
-
-
-
+if ! command -v wg &> /dev/null; then
+    # WireGuard is not installed, update and install net-tools
+    apt update -y
+    apt install net-tools -y
+fi
+clear
+wireguard_install
