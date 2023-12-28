05c4290528289ab1aeb57047559a0aba1b5ba72c
Andy <rahmanjan1989630@gmail.com>
2023-12-28 08:34:10 +0000

    a

--- WG_4_tun2socks_modified/install.sh
+++ WG_4_tun2socks_modified/install.sh
@@ -14,6 +14,7 @@ mv tun2socks-linux-amd64 /usr/bin/tun2socks
 #下载安装魔改wireguard
 curl -sSL https://raw.githubusercontent.com/andy72630/Wireguard_w_IPv6/main/WG_4_tun2socks_modified/wg_tun2socks_installer.sh | bash
 echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf > /dev/null
-curl -sSL https://raw.githubusercontent.com/andy72630/Wireguard_w_IPv6/main/WG_4_tun2socks_modified/monitor.sh | bash
+wget https://raw.githubusercontent.com/andy72630/Wireguard_w_IPv6/main/WG_4_tun2socks_modified/monitor.sh
+bash monitor.sh
 wget https://raw.githubusercontent.com/andy72630/Wireguard_w_IPv6/main/WG_4_tun2socks_modified/tunnel_setup.sh
 echo "请手动运行 bash tunnel_setup.sh start 或者 bash tunnel_setup.sh stop"
