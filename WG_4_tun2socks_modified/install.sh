58b0076b356652b3dc9c34a7c1820ab504cb7750
Andy <rahmanjan1989630@gmail.com>
2023-12-28 08:30:28 +0000

    a

--- WG_4_tun2socks_modified/install.sh
+++ WG_4_tun2socks_modified/install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-#下载安装tun2socks
+# Check if the script is running as root
+if [[ $EUID -ne 0 ]]; then
+    echo "请用sudo运行"
+    exit 1
+fi
 
+#下载安装tun2socks
 wget https://github.com/xjasonlyu/tun2socks/releases/download/v2.5.2/tun2socks-linux-amd64.zip
 unzip tun2socks-linux-amd64.zip
 mv tun2socks-linux-amd64 /usr/bin/tun2socks
 
 #下载安装魔改wireguard
-echo "1" | curl -sSL https://raw.githubusercontent.com/andy72630/Wireguard_w_IPv6/main/WG_4_tun2socks_modified/wg_tun2socks_installer.sh | bash
+curl -sSL https://raw.githubusercontent.com/andy72630/Wireguard_w_IPv6/main/WG_4_tun2socks_modified/wg_tun2socks_installer.sh | bash
+echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf > /dev/null
 curl -sSL https://raw.githubusercontent.com/andy72630/Wireguard_w_IPv6/main/WG_4_tun2socks_modified/monitor.sh | bash
 wget https://raw.githubusercontent.com/andy72630/Wireguard_w_IPv6/main/WG_4_tun2socks_modified/tunnel_setup.sh
 echo "请手动运行 bash tunnel_setup.sh start 或者 bash tunnel_setup.sh stop"
